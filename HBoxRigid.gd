e23bc7e4680cd24668b30a73f25072598e9645db
CrunchyDuck <realvulpes@gmail.com>
2024-10-13 21:11:07 +0100

    Tweaks to custom GUI nodes

--- HBoxRigid.gd
+++ HBoxRigid.gd
@@ -3,12 +3,22 @@
 class_name HBoxRigid
 extends HBoxContainer
 
-@export var fixed_width: float = 50
+var _width: float:
+	get:
+		if fixed_width > 0:
+			return fixed_width
+		return self.custom_minimum_size.x / float(fixed_elements)
+@export var fixed_width: float = 0
+@export var fixed_elements: int = 0
 
 func _process(delta: float):
+	size = custom_minimum_size
 	var children := get_children()
 	for child in children:
 		if not child is Control:
 			continue
-		child.custom_minimum_size.x = fixed_width
-		child.size.x = fixed_width
+		child.custom_minimum_size.x = _width
+		child.size.x = _width
+		child.size.y = self.custom_minimum_size.y
+		child.custom_minimum_size.y = self.custom_minimum_size.y
+			
--- VBoxRigid.gd
+++ VBoxRigid.gd
@@ -3,12 +3,21 @@
 class_name VBoxRigid
 extends VBoxContainer
 
-@export var fixed_height: float = 50
+var _height: float:
+	get:
+		if fixed_height > 0:
+			return fixed_height
+		return self.custom_minimum_size.y / float(fixed_elements)
+@export var fixed_height: float = 0
+@export var fixed_elements: int = 0
 
 func _process(delta: float):
+	self.size = custom_minimum_size
 	var children := get_children()
 	for child in children:
 		if not child is Control:
 			continue
-		child.custom_minimum_size.y = fixed_height
-		child.size.y = fixed_height
+		child.custom_minimum_size.y = _height
+		child.size.y = _height
+		child.size.x = self.custom_minimum_size.x
+		child.custom_minimum_size.x = self.custom_minimum_size.x
