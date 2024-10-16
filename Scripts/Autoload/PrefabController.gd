cca090d388167920b71d1f57f98690bac7ec4450
CrunchyDuck <realvulpes@gmail.com>
2024-10-16 20:18:03 +0100

    Change PrefabController to name prefabs by folder position

--- Scripts/Autoload/PrefabController.gd
+++ Scripts/Autoload/PrefabController.gd
@@ -3,21 +3,29 @@ extends Node
 var prefabs: Dictionary = {}
 var prefabs_filled = false
 
+func _ready():
+	fill_prefabs()
+
 func fill_prefabs():
 	var dir = DirAccess.open("res://Prefabs")
-	_parse_directory(dir)
+	_parse_directory(dir, [])
 	prefabs_filled = true
-
 		
-func _parse_directory(dir: DirAccess):
+func _parse_directory(dir: DirAccess, path: PackedStringArray):
 	var files := dir.get_files()
 	for file in files:
 		var parts = file.split(".")
 		if parts[1] == "tscn":
-			prefabs[parts[0]] = load(dir.get_current_dir() + "/" + file)
+			path.append(parts[0])
+			prefabs[".".join(path)] = load(dir.get_current_dir() + "/" + file)
+			print(".".join(path))
+			path.remove_at(path.size() - 1)
+			
 	var folders = dir.get_directories()
 	for folder in folders:
-		_parse_directory(DirAccess.open(dir.get_current_dir() + "/" + folder))
+		path.append(folder)
+		_parse_directory(DirAccess.open(dir.get_current_dir() + "/" + folder), path)
+		path.remove_at(path.size() - 1)
 	
 func get_prefab(path: String) -> PackedScene:
 	if not prefabs_filled:
