edf666b8f6c0d4de4b67d7e69281212e6b9ae321
CrunchyDuck <realvulpes@gmail.com>
2024-10-29 16:39:09 +0000

    Fix screen filter being applied to camera

--- Scripts/ColorPaletteLayer.gd
+++ Scripts/ColorPaletteLayer.gd
@@ -4,8 +4,8 @@ extends CanvasLayer
 var rect: ColorRect = $ColorRect
 
 func _ready() -> void:
-	ColorControllers.palette_updated.connect(refresh_palette)
-	refresh_palette()
+  ColorControllers.palette_updated.connect(refresh_palette)
+  refresh_palette()
 
 func refresh_palette():
-	rect.material.set_shader_parameter("palette", ColorControllers.current_palette)
+  rect.material.set_shader_parameter("palette", ColorControllers.current_palette)
