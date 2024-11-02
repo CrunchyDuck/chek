90f38487f423eae8956e80f60712f0b19044bf8b
CrunchyDuck <realvulpes@gmail.com>
2024-11-02 12:13:08 +0000

    Improve model

--- Scripts/PlayerIndicator.gd
+++ Scripts/PlayerIndicator.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-@onready var light: OmniLight3D = $Light
+@onready var light: OmniLight3D = $OmniLight3D
 @onready var mesh_instance: MeshInstance3D = self
 
 static var indicator_green: Array[Color] = [Color.hex(0x22ff00ff), Color.hex(0x22ff00ff)]
@@ -8,18 +8,18 @@ static var indicator_red: Array[Color] = [Color.hex(0xff5500ff), Color.hex(0xff5
 static var indicator_off: Array[Color] = [Color(), Color.hex(0x595959ff)]
 
 static var IndicatorColor: Dictionary = {
-	"Green": indicator_green,
-	"Red": indicator_red,
-	"Off": indicator_off,
+  "Green": indicator_green,
+  "Red": indicator_red,
+  "Off": indicator_off,
 }
 
 func _ready():
-	set_indicator(IndicatorColor["Off"])
+  set_indicator(IndicatorColor["Off"])
 
 func set_indicator(color: Array[Color]):
-	var l = color[0]
-	var m = color[1]
-	light.light_color = l
-	var material = mesh_instance.get_active_material(0)
-	material.albedo_color = m
-	mesh_instance.set_surface_override_material(0, material)
+  var l = color[0]
+  var m = color[1]
+  light.light_color = l
+  var material = mesh_instance.get_active_material(0)
+  material.albedo_color = m
+  mesh_instance.set_surface_override_material(0, material)
