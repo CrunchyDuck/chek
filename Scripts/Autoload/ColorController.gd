4450f691e777f467612fd493af93d9eba43cc982
CrunchyDuck <realvulpes@gmail.com>
2024-11-08 19:29:56 +0000

    Colour chat name by game id

--- Scripts/Autoload/ColorController.gd
+++ Scripts/Autoload/ColorController.gd
@@ -5,55 +5,55 @@ const color_count = 8
 const color_spacing = 1.0 / (color_count - 1.0)
 
 const colors = {
-	primary = Color(0, 0, 0),
-	secondary = Color(1, 1, 1),
-	c1 = Color(color_spacing * 1, color_spacing * 1, color_spacing * 1),
-	c2 = Color(color_spacing * 2, color_spacing * 2, color_spacing * 2),
-	c3 = Color(color_spacing * 3, color_spacing * 3, color_spacing * 3),
-	c4 = Color(color_spacing * 4, color_spacing * 4, color_spacing * 4),
-	c5 = Color(color_spacing * 5, color_spacing * 5, color_spacing * 5),
-	c6 = Color(color_spacing * 6, color_spacing * 6, color_spacing * 6),
+  primary = Color(0, 0, 0),
+  secondary = Color(1, 1, 1),
+  c1 = Color(color_spacing * 1, color_spacing * 1, color_spacing * 1),
+  c2 = Color(color_spacing * 2, color_spacing * 2, color_spacing * 2), # Background color
+  c3 = Color(color_spacing * 3, color_spacing * 3, color_spacing * 3),
+  c4 = Color(color_spacing * 4, color_spacing * 4, color_spacing * 4),
+  c5 = Color(color_spacing * 5, color_spacing * 5, color_spacing * 5),
+  c6 = Color(color_spacing * 6, color_spacing * 6, color_spacing * 6),
 }
 
 var current_palette: CompressedTexture2D:
-	get:
-		return palettes[current_palette_name]
+  get:
+    return palettes[current_palette_name]
 var current_palette_name: String:
-	get:
-		return palettes.keys()[_current_index]
+  get:
+    return palettes.keys()[_current_index]
 var palettes: Dictionary = {}
 var _current_index: int = 0
 
 signal palette_updated
 
 func _ready():
-	_load_palettes()
-	
+  _load_palettes()
+  
 func _input(event):
-	if event.is_action_pressed("PreviousPalette"):
-		previous_palette()
-	elif event.is_action_pressed("NextPalette"):
-		next_palette()
+  if event.is_action_pressed("PreviousPalette"):
+    previous_palette()
+  elif event.is_action_pressed("NextPalette"):
+    next_palette()
 
 func _load_palettes():
-	palettes = {}
-	var dir = DirAccess.open("res://Sprites/Palettes/")
-	var files := dir.get_files()
-	for file in files:
-		var parts = file.split(".")
-		if parts[-1] == "png":
-			var p = load(dir.get_current_dir() + "/" + file)
-			palettes[parts[0]] = load(dir.get_current_dir() + "/" + file)
-	
-	palette_updated.emit()
+  palettes = {}
+  var dir = DirAccess.open("res://Sprites/Palettes/")
+  var files := dir.get_files()
+  for file in files:
+    var parts = file.split(".")
+    if parts[-1] == "png":
+      var p = load(dir.get_current_dir() + "/" + file)
+      palettes[parts[0]] = load(dir.get_current_dir() + "/" + file)
+  
+  palette_updated.emit()
 
 func previous_palette():
-	_current_index -= 1
-	_current_index = wrapi(_current_index, 0, palettes.size())
-	palette_updated.emit()
-	
+  _current_index -= 1
+  _current_index = wrapi(_current_index, 0, palettes.size())
+  palette_updated.emit()
+  
 func next_palette():
-	_current_index += 1
-	_current_index = wrapi(_current_index, 0, palettes.size())
-	palette_updated.emit()
-	pass
+  _current_index += 1
+  _current_index = wrapi(_current_index, 0, palettes.size())
+  palette_updated.emit()
+  pass
--- Scripts/Autoload/MessageController.gd
+++ Scripts/Autoload/MessageController.gd
@@ -18,12 +18,39 @@ func _ready():
 func _on_submit(new_text: String):
   var content = node_input.text
   node_input.clear()
-  # TODO: Send to network
-  add_message(content)
+  var my_color: String = ColorControllers.colors["c1"].to_html()
+  if multiplayer.get_peers().size() != 0:
+    var id = GameController.players_by_net_id[multiplayer.get_unique_id()].game_id
+    my_color = ColorControllers.colors["c" + str(id + 3)].to_html()
+  var message_values = {
+      "name_color": my_color,
+      "name": GameController.character_name,
+      "content": content,
+      "content_color": ColorControllers.colors.secondary.to_html(),
+    }
+  var message = "[b][color={name_color}]{name}:[/color][/b] [color={content_color}]{content}[/color]"
+  message = message.format(message_values)
+  # Is there really no way to check if you're connected to a server??
+  if multiplayer.get_peers().size() != 0:
+    try_add_message.rpc_id(1, message)
+  else:
+    add_message(message)
+  
+@rpc("any_peer", "call_local", "reliable", 1)
+func try_add_message(message: String):
+  if not multiplayer.is_server():
+    return
+  if message.length() > 512:
+    return
+  add_message.rpc(message)
   
 @rpc("authority", "call_local", "reliable", 1)
 func add_message(message: String):
-  var node_message = Label.new()
+  var node_message = RichTextLabel.new()
   node_message.text = message
+  node_message.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+  node_message.scroll_active = false
+  node_message.bbcode_enabled = true
+  node_message.fit_content = true
   node_message_list.add_child(node_message)
   
