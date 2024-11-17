b8163f78f7b12756a6180ace845fae79a0c518dd
CrunchyDuck <realvulpes@gmail.com>
2024-11-17 15:30:48 +0000

    Make IP fetching wait for response

--- Prefabs/Menus/LobbyHosting.gd
+++ Prefabs/Menus/LobbyHosting.gd
@@ -11,20 +11,20 @@ var input_ip: LineEdit = $CenterContainer/VBoxContainer/HBoxContainer/LineEdit
 var input_port: LineEdit = $CenterContainer/VBoxContainer/HBoxContainer/LineEdit2
 
 func _ready() -> void:
-	# TODO: For some reason the back button doesn't work here.
-	button_back.pressed.connect(_on_back)
-	button_done.pressed.connect(_on_done)
-	input_ip.editable = false  # TODO: Fetch IP from internet.
-	if not GameController.public_ip.is_empty():
-		input_ip.text = GameController.public_ip
+  # TODO: For some reason the back button doesn't work here.
+  button_back.pressed.connect(_on_back)
+  button_done.pressed.connect(_on_done)
+  input_ip.editable = false  # TODO: Fetch IP from internet.
+  if not GameController.public_ip.is_empty():
+    input_ip.text = GameController.public_ip
 
 func _on_back():
-	$"..".add_child(PrefabController.get_prefab("Menus.Start").instantiate())
-	queue_free()
-	
+  $"..".add_child(PrefabController.get_prefab("Menus.Start").instantiate())
+  queue_free()
+  
 func _on_done():
-	if not GameController.start_lobby(int(input_port.text)):
-		# TODO: Print error in chat
-		return
-	$"..".add_child(PrefabController.get_prefab("Menus.GameSetup").instantiate())
-	queue_free()
+  if not GameController.start_lobby(int(input_port.text)):
+    # TODO: Print error in chat
+    return
+  $"..".add_child(PrefabController.get_prefab("Menus.GameSetup").instantiate())
+  queue_free()
