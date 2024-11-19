892aea9489b2785e2a1f5a27f95e48e8feeb7398
CrunchyDuck <realvulpes@gmail.com>
2024-11-19 14:46:44 +0000

    Add lever action

---
+++ Scripts/Model/LeverBehavior.gd
@@ -0,0 +1,87 @@
+extends Node3D
+
+@onready
+var node_handle: Node3D = $Handle
+@onready
+var node_arms: Node3D = $Arms
+@onready
+var node_pivot: Node3D = $Pivot
+
+@onready
+var node_click_area: Area3D = $ClickArea
+
+@onready
+var node_on_off_light: OmniLight3D
+
+
+# Grabbing and pulling lever
+var on = false
+var tripping = false
+var being_held: bool = false
+var lever_rotation_target: float = 0
+var lever_rotation: float = 0
+var lever_actual: float = 0  # Where it is after being clamped
+var lever_catch: float = 45
+var lever_trip: float = 90
+
+var lever_normal_speed: float = 200
+var lever_trip_speed: float = 900
+var lever_max_speed: float = lever_normal_speed
+
+var delay_room_light: float = 2
+var delay_screens: float = 1
+
+var sound_switch
+var sound_light_on
+var sound_fan
+
+func _ready():
+  node_click_area.input_event.connect(_mouse_input_event)
+
+func turn_on():
+  on = true
+  being_held = false
+  tripping = true
+  lever_max_speed = lever_trip_speed
+  
+func turn_off():
+  on = false
+  being_held = false
+  tripping = true
+  lever_max_speed = lever_trip_speed
+
+func _process(delta: float) -> void:
+  if not Input.is_action_pressed("LMB"):
+    being_held = false
+  if not being_held:
+    if not on:
+      lever_rotation_target = 0
+    else:
+      lever_rotation_target = 180
+  
+  lever_rotation_target = clampf(lever_rotation_target, 0, 180)
+  # Trigger thresholds
+  if not tripping:
+    if not on and lever_rotation >= lever_trip:
+      turn_on()
+    elif on and lever_rotation <= 180 - lever_trip:
+      turn_off()
+      
+  lever_rotation = move_toward(lever_rotation, lever_rotation_target, lever_max_speed * delta)
+  lever_actual = lever_rotation
+  if not tripping:
+    if not on:
+      lever_actual = min(lever_rotation, lever_catch)
+    else:
+      lever_actual = max(lever_rotation, 180 - lever_catch)
+  node_arms.rotation_degrees.x = -lever_actual
+  node_pivot.rotation_degrees.x = -lever_actual
+  node_handle.rotation_degrees.x = -lever_actual
+
+func _input(event):
+  if being_held and event is InputEventMouseMotion:
+    lever_rotation_target += event.relative.y
+
+func _mouse_input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+  if not tripping and Input.is_action_pressed("LMB"):
+    being_held = true
--- Scripts/ScreenButton.gd
+++ Scripts/ScreenButton.gd
@@ -5,11 +5,11 @@ extends Node
 @export var animator: AnimationPlayer
 
 var node_collider: Area3D:
-	get:
-		return $Area3D
+  get:
+    return $Area3D
 var node_light: OmniLight3D:
-	get:
-		return $OmniLight3D
+  get:
+    return $OmniLight3D
 
 var _light_energy: float = 1
 
@@ -17,33 +17,33 @@ var _pressed: bool = false
 signal on_pressed(button: ScreenButton)
 
 func _ready():
-	node_collider.input_event.connect(_mouse_input_event)
-	node_collider.mouse_exited.connect(depress)
-	
+  node_collider.input_event.connect(_mouse_input_event)
+  node_collider.mouse_exited.connect(depress)
+  
 func _process(_delta: float) -> void:
-	if on_pressed.get_connections().size() == 0:
-		node_light.light_energy = 0
-	else:
-		node_light.light_energy = _light_energy
-	
+  if on_pressed.get_connections().size() == 0:
+    node_light.light_energy = 0
+  else:
+    node_light.light_energy = _light_energy
+  
 func _mouse_input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.pressed:
-			press()
-		else:
-			depress()
-	
+  if event is InputEventMouseButton:
+    if event.pressed:
+      press()
+    else:
+      depress()
+  
 func press():
-	if _pressed:
-		return
-	on_pressed.emit(self)
-	animator.play(direction + "Press")
-	_pressed = true
-	# TODO: Play sound
+  if _pressed:
+    return
+  on_pressed.emit(self)
+  animator.play(direction + "Press")
+  _pressed = true
+  # TODO: Play sound
 
 func depress():
-	if not _pressed:
-		return
-	animator.play(direction + "Depress")
-	_pressed = false
-	# TODO: Play sound
+  if not _pressed:
+    return
+  animator.play(direction + "Depress")
+  _pressed = false
+  # TODO: Play sound
