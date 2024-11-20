da51ade26aa9a140391cc919b74763420a13d679
CrunchyDuck <realvulpes@gmail.com>
2024-11-20 17:36:44 +0000

    Add sound to switching fan on/off

--- Scripts/Menus/Setup/SetupRules.gd
+++ Scripts/Menus/Setup/SetupRules.gd
@@ -2,10 +2,10 @@ class_name SetupMenu
 extends Control
 
 var settings: BoardBase.GameSettings:
-	get:
-		return GameController.game_settings
-	set(value):
-		GameController.game_settings = value
+  get:
+    return GameController.game_settings
+  set(value):
+    GameController.game_settings = value
 
 @onready
 var button_start: Button = $Start
@@ -18,38 +18,38 @@ var button_no_retreat: CheckBox = $Settings/NoRetreat/CheckBox
 var button_next_screen: ScreenButton = $/root/MainScene/Console/front_panel/arrow_right
 
 func _ready() -> void:
-	button_start.pressed.connect(_on_start)
-	if not multiplayer.is_server():
-		button_start.disabled = true
-	
-	button_divine_wind.pressed.connect(_on_change)
-	button_no_retreat.pressed.connect(_on_change)
-	settings = gather_settings()
-	button_next_screen.on_pressed.connect(_next_screen)
+  button_start.pressed.connect(_on_start)
+  if not multiplayer.is_server():
+    button_start.disabled = true
+  
+  button_divine_wind.pressed.connect(_on_change)
+  button_no_retreat.pressed.connect(_on_change)
+  settings = gather_settings()
+  button_next_screen.on_pressed.connect(_next_screen)
 
 func _next_screen(button) -> void:
-	print("here")
-	# Move to board setup
+  print("here")
+  # Move to board setup
 
 func _on_change():
-	load_settings.rpc(gather_settings().serialize())
-	
+  load_settings.rpc(gather_settings().serialize())
+  
 func _on_start():
-	settings = gather_settings()
-	
-	var jgs = settings.serialize()
-	var jbs = GameController.standard_board_setup().serialize()
-	GameController.start_game.rpc(jgs, jbs)
-	
+  settings = gather_settings()
+  
+  var jgs = settings.serialize()
+  var jbs = GameController.standard_board_setup().serialize()
+  GameController.start_game.rpc(jgs, jbs)
+  
 func gather_settings() -> BoardBase.GameSettings:
-	var settings = BoardBase.GameSettings.new()
-	settings.divine_wind = button_divine_wind.button_pressed
-	settings.no_retreat = button_no_retreat.button_pressed
-	return settings
+  var settings = BoardBase.GameSettings.new()
+  settings.divine_wind = button_divine_wind.button_pressed
+  settings.no_retreat = button_no_retreat.button_pressed
+  return settings
 
 @rpc("any_peer", "call_local", "reliable", 0)
 func load_settings(json_settings: Dictionary):
-	settings = BoardBase.GameSettings.deserialize(json_settings)
-	button_divine_wind.button_pressed = settings.divine_wind
-	button_no_retreat.button_pressed = settings.no_retreat
-	
+  settings = BoardBase.GameSettings.deserialize(json_settings)
+  button_divine_wind.button_pressed = settings.divine_wind
+  button_no_retreat.button_pressed = settings.no_retreat
+  
--- Scripts/Model/FanBehavior.gd
+++ Scripts/Model/FanBehavior.gd
@@ -5,76 +5,83 @@ extends Node3D
 var node_off_area: Area3D = $OffArea
 @onready
 var node_on_area: Area3D = $OnArea
-
 @onready
 var node_button: Node3D = $Button
+
+@onready
+var sound_fan: FmodEventEmitter3D = $SoundRunning
+@onready
+var sound_switch: FmodEventEmitter3D = $SoundSwitch
+
 var button_rotation: float = 8
 
 var on: bool: 
-	get: 
-		return self_on and console_on
+  get: 
+    return self_on and console_on
 var self_on: bool = true
 var console_on: bool = false
 var fan_running: bool = false
 
-@onready
-var fan_sound: FmodEventEmitter3D = $Sound
 var last_on: float = 0
 var sound_target: float = 0
 var settle_volume: float = 0.1
 var settle_time: float = 20
 var fade_speed: float:
-	get:
-		return 0.2 if on else 0.5
+  get:
+    return 0.2 if on else 0.5
 var on_delay = 1
 
 func _ready() -> void:
-	node_off_area.input_event.connect(_off)
-	node_on_area.input_event.connect(_on)
-	$"../Lever".switched_on.connect(func (): console_switch(true))
-	$"../Lever".switched_off.connect(func (): console_switch(false))
-	fan_sound.volume = 0
-	
+  node_off_area.input_event.connect(_off)
+  node_on_area.input_event.connect(_on)
+  $"../Lever".switched_on.connect(func (): console_switch(true))
+  $"../Lever".switched_off.connect(func (): console_switch(false))
+  sound_fan.volume = 0
+  
 func _process(delta: float) -> void:
-	if fan_running:
-		var time_on = (Time.get_ticks_msec() / 1000.0) - last_on
-		var t = clamp(time_on / settle_time, 0, 1)
-		sound_target = lerpf(settle_volume, 1, 1 - t)
-	else:
-		sound_target = 0
-		
-	fan_sound.volume = move_toward(fan_sound.volume, sound_target, fade_speed * delta)
-	if fan_sound.volume == 0:
-		fan_running = false
-		fan_sound.stop()
-		
+  if fan_running:
+    var time_on = (Time.get_ticks_msec() / 1000.0) - last_on
+    var t = clamp(time_on / settle_time, 0, 1)
+    sound_target = lerpf(settle_volume, 1, 1 - t)
+  else:
+    sound_target = 0
+    
+  sound_fan.volume = move_toward(sound_fan.volume, sound_target, fade_speed * delta)
+  if sound_fan.volume == 0:
+    fan_running = false
+    sound_fan.stop()
+    
 func console_switch(state):
-	console_on = state
-	if on:
-		await get_tree().create_timer(on_delay).timeout
-	update_sound()
-	
+  console_on = state
+  if on:
+    await get_tree().create_timer(on_delay).timeout
+  update_sound()
+  
 func update_sound():
-	if on:
-		last_on = Time.get_ticks_msec() / 1000
-		if fan_sound.volume == 0:
-			fan_running = true
-			fan_sound.volume = 1
-			fan_sound.play()
-	else:
-		fan_running = false
+  if on:
+    last_on = Time.get_ticks_msec() / 1000
+    if sound_fan.volume == 0:
+      fan_running = true
+      sound_fan.volume = 1
+      sound_fan.play()
+  else:
+    fan_running = false
 
 # TODO: Fan switch sounds
 func _off(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int):
-	if not event.is_action_pressed("LMB"):
-		return
-	node_button.rotation_degrees.x = -button_rotation
-	self_on = false
-	update_sound()
-	
+  if not event.is_action_pressed("LMB") or not self_on:
+    return
+  sound_switch.stop()
+  sound_switch.play()
+  node_button.rotation_degrees.x = -button_rotation
+  self_on = false
+  update_sound()
+  
 func _on(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int):
-	if not event.is_action_pressed("LMB"):
-		return
-	node_button.rotation_degrees.x = button_rotation
-	self_on = true
-	update_sound()
+  if not event.is_action_pressed("LMB") or self_on:
+    return
+  sound_switch.stop()
+  sound_switch.play()
+  node_button.rotation_degrees.x = button_rotation
+  self_on = true
+  update_sound()
