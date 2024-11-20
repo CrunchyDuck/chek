f9a2cd751a03df64a4926d9dc463700ae8648f47
CrunchyDuck <realvulpes@gmail.com>
2024-11-20 09:43:13 +0000

    Clean up lever code

--- Scripts/Model/LeverBehavior.gd
+++ Scripts/Model/LeverBehavior.gd
@@ -20,15 +20,19 @@ var node_console: Node3D = $".."
 var on = false
 var tripping = false
 var being_held: bool = false
-var lever_rotation_target: float = 0
-var lever_rotation: float = 0
-var lever_actual: float = 0  # Where it is after being clamped
+var mouse_pull: float = 0  # Where the mouse is trying to pull the lever
+var lever_rest_position: float:
+  get:
+    return 180 if on else 0
+var lever_rotation: float = 0  # The lever's progress, being being slowed.
 var lever_trip: float = 50
 var mouse_movement_to_rotation = 0.2
 
 var lever_normal_speed: float = 100
 var lever_trip_speed: float = 1200
-var lever_max_speed: float = lever_normal_speed
+var lever_speed: float:
+  get:
+    return lever_normal_speed if not tripping else lever_trip_speed
 
 var on_time: float = 0
 var delay_room_light: float = 2
@@ -47,8 +51,7 @@ func _ready():
 func turn_on():
   on = true
   being_held = false
-  tripping = true
-  lever_max_speed = lever_trip_speed
+  tripping = true  # A period of fast movement and immunity.
   sound_switch.play()
   switched_on.emit()
   await get_tree().create_timer(tripping_time).timeout
@@ -58,48 +61,47 @@ func turn_off():
   on = false
   being_held = false
   tripping = true
-  lever_max_speed = lever_trip_speed
   sound_switch.play()
   switched_off.emit()
   await get_tree().create_timer(tripping_time).timeout
   tripping = false
 
 func _process(delta: float) -> void:
-  if not Input.is_action_pressed("LMB"):
-    being_held = false
-  if not being_held:
-    if not on:
-      lever_rotation_target = 0
-    else:
-      lever_rotation_target = 180
+  var lever_rotation_target = lever_rest_position + mouse_pull
+  lever_rotation = move_toward(lever_rotation, lever_rotation_target, lever_speed * delta)
+  lever_rotation = clampf(lever_rotation, 0, 180)
   
-  lever_rotation_target = clampf(lever_rotation_target, 0, 180)
-  # Trigger thresholds
+  # Trip thresholds
   if not tripping:
     if not on and lever_rotation >= lever_trip:
       turn_on()
     elif on and lever_rotation <= 180 - lever_trip:
       turn_off()
       
-  lever_rotation = move_toward(lever_rotation, lever_rotation_target, lever_max_speed * delta)
-  lever_actual = lever_rotation
+  # Dampening
+  var lever_dampened = lever_rotation
   if not tripping:
     if not on:
       var t = lever_rotation / lever_trip
       t = pow(t, 0.5)
-      lever_actual = lerpf(0, lever_trip, t)
+      lever_dampened = lerpf(0, lever_trip, t)
     else:
-      var t = lever_rotation / (180 - lever_trip)
+      var t = 1 - (lever_rotation / lever_trip)
       t = pow(t, 0.5)
-      lever_actual = lerpf(0, lever_trip, t) + 180
-  node_arms.rotation_degrees.x = -lever_actual
-  node_pivot.rotation_degrees.x = -lever_actual
-  node_handle.rotation_degrees.x = -lever_actual
+      lever_dampened = lerpf(0, lever_trip, t)
+      
+  node_arms.rotation_degrees.x = -lever_dampened
+  node_pivot.rotation_degrees.x = -lever_dampened
+  node_handle.rotation_degrees.x = -lever_dampened
 
 func _input(event):
+  if not Input.is_action_pressed("LMB"):
+    being_held = false
+    mouse_pull = 0
   if being_held and event is InputEventMouseMotion:
-    lever_rotation_target += event.relative.y * mouse_movement_to_rotation
+    mouse_pull += event.relative.y * mouse_movement_to_rotation
 
 func _mouse_input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
   if Input.is_action_just_pressed("LMB"):
+    mouse_pull = 0
     being_held = true
