394228e46100e79673789c1c9203ea8106f39f70
CrunchyDuck <realvulpes@gmail.com>
2024-11-22 13:25:06 +0000

    Fix broken prefab reference

--- Prefabs/Menus/LobbyJoining.gd
+++ Prefabs/Menus/LobbyJoining.gd
@@ -16,36 +16,36 @@ var input_ip: LineEdit = $CenterContainer/VBoxContainer/HBoxContainer/LineEdit
 var input_port: LineEdit = $CenterContainer/VBoxContainer/HBoxContainer/LineEdit2
 
 func _ready() -> void:
-  button_back.pressed.connect(_on_back)
-  button_done.pressed.connect(_on_done)
-  
-  multiplayer.connection_failed.connect(_on_fail)
-  multiplayer.connected_to_server.connect(_on_success)
+	button_back.pressed.connect(_on_back)
+	button_done.pressed.connect(_on_done)
+	
+	multiplayer.connection_failed.connect(_on_fail)
+	multiplayer.connected_to_server.connect(_on_success)
 
 func _on_back():
-  if info_page.visible:
-    $"..".add_child(PrefabController.get_prefab("Menus.Start").instantiate())
-    queue_free()
-    return
-  info_page.visible = true
-  connecting_page.visible = false
-  multiplayer.multiplayer_peer = null
-  
+	if info_page.visible:
+		$"..".add_child(PrefabController.get_prefab("Menus.Start").instantiate())
+		queue_free()
+		return
+	info_page.visible = true
+	connecting_page.visible = false
+	multiplayer.multiplayer_peer = null
+	
 func _on_done():
-  if not GameController.join_lobby(input_ip.text, int(input_port.text)):
-    # TODO: Chat message detailing fail
-    return
-  connecting_page.visible = true
-  info_page.visible = false
-  print("connecting")
-  
+	if not GameController.join_lobby(input_ip.text, int(input_port.text)):
+		# TODO: Chat message detailing fail
+		return
+	connecting_page.visible = true
+	info_page.visible = false
+	print("connecting")
+	
 func _on_success():
-  $"..".add_child(PrefabController.get_prefab("Menus.GameSetup").instantiate())
-  queue_free()
-  print("connected")
-  
+	$"..".add_child(PrefabController.get_prefab("Menus.Setup.Main").instantiate())
+	queue_free()
+	print("connected")
+	
 func _on_fail():
-  connecting_page.visible = false
-  info_page.visible = true
-  print("failed to connect")
-  # TODO: Print error in chat.
+	connecting_page.visible = false
+	info_page.visible = true
+	print("failed to connect")
+	# TODO: Print error in chat.
