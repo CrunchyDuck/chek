a1237e31e04e61bc3b57f998abf932c0e0d2c900
CrunchyDuck <realvulpes@gmail.com>
2024-11-23 12:02:35 +0000

    Add ability to change piece in board editor

--- Scripts/Menus/BoardEditorSecondaryScreen.gd
+++ Scripts/Menus/BoardEditorSecondaryScreen.gd
@@ -2,6 +2,9 @@ class_name BoardEditorSecondaryScreen
 extends Control
 
 @onready
+var screen_controller: SecondaryScreenController = $"/root/MainScene/ViewportRightScreen/SecondaryScreenController"
+
+@onready
 var node_piece_position: Control = $Piece	
 var piece: ChessPiece
 @onready
@@ -16,41 +19,67 @@ var orientation: ChessPiece.Orientation = ChessPiece.Orientation.North
 var player: int = 0
 
 const orientation_string = {
-	ChessPiece.Orientation.North: "UP",
-	ChessPiece.Orientation.East: "RIGHT",
-	ChessPiece.Orientation.South: "DOWN",
-	ChessPiece.Orientation.West: "LEFT",
+  ChessPiece.Orientation.North: "UP",
+  ChessPiece.Orientation.East: "RIGHT",
+  ChessPiece.Orientation.South: "DOWN",
+  ChessPiece.Orientation.West: "LEFT",
 }
 
 const player_string = {
-	0: "COMMANDER 0",
-	1: "COMMANDER 1",
-	2: "COMMANDER 2",
-	3: "COMMANDER 3",
+  0: "COMMANDER 0",
+  1: "COMMANDER 1",
+  2: "COMMANDER 2",
+  3: "COMMANDER 3",
+}
+
+const player_orientation_default = {
+  0: ChessPiece.Orientation.North,
+  1: ChessPiece.Orientation.South,
+  2: ChessPiece.Orientation.East,
+  3: ChessPiece.Orientation.West,
 }
 
 func _ready():
-	node_description.bbcode_enabled = true
+  node_description.bbcode_enabled = true
+  screen_controller.left_button[0].on_pressed.connect(prev_piece)
+  screen_controller.right_button[0].on_pressed.connect(next_piece)
+
+#region Button events
+func prev_piece():
+  var i = int(piece.piece_type) - 1
+  if i < 0:
+    i = ChessPiece.ePieces.size() - 1
+  var new_piece = ChessPiece.piece_prefabs[i as ChessPiece.ePieces]
+  set_piece(new_piece)
+  
+func next_piece():
+  var i = int(piece.piece_type) + 1
+  if i >= ChessPiece.ePieces.size():
+    i = 0
+  var new_piece = ChessPiece.piece_prefabs[i as ChessPiece.ePieces]
+  set_piece(new_piece)
+#endregion
 
 func set_piece(_piece_prefab: String):
-	for n in node_piece_position.get_children():
-		n.queue_free()
-	
-	piece = PrefabController.get_prefab(_piece_prefab).instantiate()
-	node_piece_position.add_child(piece)
-	node_description.text = piece.piece_description
-	piece.orientation = orientation
-	piece.owned_by = player
-	editor.paint_piece = piece.serialize()
+  for n in node_piece_position.get_children():
+    n.queue_free()
+  
+  piece = PrefabController.get_prefab(_piece_prefab).instantiate()
+  node_piece_position.add_child(piece)
+  node_description.text = piece.piece_description
+  piece.orientation = orientation
+  piece.owned_by = player
+  editor.paint_piece = piece.serialize()
 
 func set_orientation(_orientation: ChessPiece.Orientation):
-	orientation = _orientation
-	piece.orientation = orientation
-	node_orientation.text = orientation_string[orientation]
-	editor.paint_piece = piece.serialize()
-	
+  orientation = _orientation
+  piece.orientation = orientation
+  node_orientation.text = orientation_string[orientation]
+  editor.paint_piece = piece.serialize()
+  
 func set_player(_player: int):
-	player = _player
-	piece.owned_by = player
-	node_team.text = player_string[player]
-	editor.paint_piece = piece.serialize()
+  player = _player
+  piece.owned_by = player
+  node_team.text = player_string[player]
+  editor.paint_piece = piece.serialize()
+  set_orientation(player_orientation_default[_player])
--- Scripts/Menus/Setup/BoardEditor.gd
+++ Scripts/Menus/Setup/BoardEditor.gd
@@ -6,18 +6,17 @@ var editor_screen: BoardEditorSecondaryScreen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Spawn a board
-	board = PrefabController.spawn_prefab("Board.BoardEditable", str(get_path()) + "/Board")
-	board.load_state(GameController.board_state)
-	
-	# Set side screen to have information panel
-	for n in GameController.screen_secondary.get_children():
-		n.queue_free()
-	
-	editor_screen = PrefabController.get_prefab("SecondaryScreen.BoardEditor").instantiate()
-	GameController.screen_secondary.add_child(editor_screen)
-	editor_screen.editor = board
-	editor_screen.set_piece(ChessPiece.piece_prefabs[ChessPiece.ePieces.Pawn])
-	editor_screen.set_orientation(ChessPiece.Orientation.South)
-	editor_screen.set_player(1)
-	
+  # Spawn a board
+  board = PrefabController.spawn_prefab("Board.BoardEditable", str(get_path()) + "/Board")
+  board.load_state(GameController.board_state)
+  
+  # Set side screen to have information panel
+  for n in GameController.screen_secondary.get_children():
+    n.queue_free()
+  
+  editor_screen = PrefabController.get_prefab("SecondaryScreen.BoardEditor").instantiate()
+  GameController.screen_secondary.add_child(editor_screen)
+  editor_screen.editor = board
+  editor_screen.set_piece(ChessPiece.piece_prefabs[ChessPiece.ePieces.Pawn])
+  editor_screen.set_player(0)
+  
