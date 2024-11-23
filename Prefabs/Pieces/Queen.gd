590becfa5ac73616f2e7039acd46496b92eb4ff3
CrunchyDuck <realvulpes@gmail.com>
2024-11-23 11:55:50 +0000

    Make pieces identify themselves

--- Prefabs/Pieces/Queen.gd
+++ Prefabs/Pieces/Queen.gd
@@ -3,11 +3,14 @@ extends ChessPiece
 var piece_description = ColorControllers.color_text("Spirit of Motherland.\n", ColorControllers.description_color)\
 + "Move and take any direction, any distance."
 
+func _ready():
+  piece_type = ePieces.Queen
+  
 func _get_actions() -> Array[BoardPlayable.GameAction]:
-	var actions: Array[BoardPlayable.GameAction] = []
-	for x in range(-1, 2):
-		for y in range(-1, 2):
-			if x == 0 and y == 0:
-				continue
-			actions += _act_in_line(Vector2i(x, y))
-	return actions
+  var actions: Array[BoardPlayable.GameAction] = []
+  for x in range(-1, 2):
+    for y in range(-1, 2):
+      if x == 0 and y == 0:
+        continue
+      actions += _act_in_line(Vector2i(x, y))
+  return actions
--- Prefabs/Pieces/Rook.gd
+++ Prefabs/Pieces/Rook.gd
@@ -3,10 +3,13 @@ extends ChessPiece
 var piece_description = ColorControllers.color_text("Self destructive vendetta.\n", ColorControllers.description_color)\
 + "Move and take cardinals."
 
+func _ready():
+  piece_type = ePieces.Rook
+
 func _get_actions() -> Array[BoardPlayable.GameAction]:
-	var actions: Array[BoardPlayable.GameAction] = []
-	actions += _act_in_line(Vector2i(0, 1))
-	actions += _act_in_line(Vector2i(0, -1))
-	actions += _act_in_line(Vector2i(1, 0))
-	actions += _act_in_line(Vector2i(-1, 0))
-	return actions
+  var actions: Array[BoardPlayable.GameAction] = []
+  actions += _act_in_line(Vector2i(0, 1))
+  actions += _act_in_line(Vector2i(0, -1))
+  actions += _act_in_line(Vector2i(1, 0))
+  actions += _act_in_line(Vector2i(-1, 0))
+  return actions
