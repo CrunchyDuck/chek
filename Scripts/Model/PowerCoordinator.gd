7e064174efae2bbe242cbb7e73e4cd63bfb0f760
CrunchyDuck <realvulpes@gmail.com>
2024-11-24 20:28:29 +0000

    Add a function for setting power management

--- Scripts/Model/PowerCoordinator.gd
+++ Scripts/Model/PowerCoordinator.gd
@@ -20,20 +20,26 @@ var last_updated_state: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   var power_lever: LeverBehavior = $"/root/MainScene/Console/Lever"
-  power_lever.switched_on.connect(func (): toggle_console(true))
-  power_lever.switched_off.connect(func (): toggle_console(false))
+  power_lever.switched_on.connect(func (): set_console(true))
+  power_lever.switched_off.connect(func (): set_console(false))
   
   if power_switch:
     power_switch.on_pressed.connect(func (): toggle_self())
   if power_to:
     power_to.toggle_power(on)
 
-func toggle_console(state):
+func set_console(state):
   console_on = state
   try_change_state()
 
 func toggle_self():
   self_on = not self_on
+  try_change_state()
+  
+func set_self(state):
+  if state == self_on:
+    return
+  self_on = state
   try_change_state()
 
 func try_change_state():
