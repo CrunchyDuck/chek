e4ba21194fdafbc64e68c80c9e5d211021ff9365
CrunchyDuck <realvulpes@gmail.com>
2024-11-26 11:46:42 +0000

    Add BoardCoordinates class

---
+++ Prefabs/Board/BoardCoordinates.gd
@@ -0,0 +1,29 @@
+class_name BoardCoordinates
+extends Control
+
+@onready
+var numbers_container: Control = $Coordinates
+var coordinates_size: Vector2:
+	get:
+		return BoardBase.coordinates_size
+
+var direction: Vector2
+
+func create_coordinates(_count: int):
+	for _n in get_children():
+		remove_child(_n)
+		_n.queue_free()
+		
+	for i in range(_count):
+		_spawn_number(i)
+	
+func _spawn_number(_number: int):
+	var _l = Label.new()
+	numbers_container.add_child(_l)
+	_l.size = coordinates_size	
+	_l.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	_l.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	_l.position = coordinates_size * direction * _number
+
+func scroll_to(scroll_number: int):
+	numbers_container.position = direction * coordinates_size * -scroll_number
