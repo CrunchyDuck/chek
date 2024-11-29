df988a5415eca4cd32728ab975cf14a6de7189a6
CrunchyDuck <realvulpes@gmail.com>
2024-11-29 19:23:09 +0000

    Make presets generate in list

--- Scripts/Menus/Setup/SetupPreset.gd
+++ Scripts/Menus/Setup/SetupPreset.gd
@@ -4,8 +4,12 @@ extends Node
 const presets_path: String = "res://BoardPresets"
 var presets: Array[BoardBase.GamePreset] = []
 
+@onready
+var node_preset_list: VBoxContainer = $PresetsViewport/PresetList
+
 func _ready():
 	read_presets()
+	create_entries()
 
 func read_presets():
 	presets = []
@@ -13,7 +17,27 @@ func read_presets():
 	
 	var files = dir.get_files()
 	for file in files:
-		var f = FileAccess.open(presets_path + "/" + file, FileAccess.READ)
-		var j = JSON.parse_string(f.get_line())
+		var f = FileAccess.get_file_as_string(presets_path + "/" + file)
+		var j = JSON.parse_string(f)
 		var gp = BoardBase.GamePreset.deserialize(j)
 		presets.append(gp)
+
+func create_entries():
+	for n in node_preset_list.get_children():
+		node_preset_list.remove_child(n)
+		
+	for p in presets:
+		create_entry(p)
+	
+func create_entry(preset: BoardBase.GamePreset):
+	var p = PrefabController.get_prefab("Menus.Setup.PresetEntry").instantiate()
+	p.get_node("Fields/Name").text = preset.name
+	p.get_node("Fields/Player").text = str(preset.players)
+	p.get_node("Fields/Complexity").text = str(preset.complexity)
+	p.get_node("Button").pressed.connect(func (): set_preset(preset))
+	node_preset_list.add_child(p)
+
+func set_preset(preset: BoardBase.GamePreset):
+	# TODO: Highlight currently selected prefab.
+	# TODO: Remove prefab highlight when any change happens
+	pass
