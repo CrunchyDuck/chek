dcddd1e9bcd7e68265df7d0df0a37ad9c8c67758
CrunchyDuck <realvulpes@gmail.com>
2024-12-02 02:35:14 +0000

    Styling for victory screen

--- Scripts/Menus/VictoryScreen.gd
+++ Scripts/Menus/VictoryScreen.gd
@@ -3,7 +3,6 @@ extends Node
 @onready
 var button_return: Button = $CenterContainer/VBoxContainer/Button
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button_return.pressed.connect(_on_return)
 	
