08eb7ce2eac4054835e68e4a28dfa5504fd13a0d
CrunchyDuck <realvulpes@gmail.com>
2024-12-02 16:36:02 +0000

    VictoryScreen controller code

--- Scripts/Menus/VictoryScreen.gd
+++ Scripts/Menus/VictoryScreen.gd
@@ -2,9 +2,22 @@ extends Node
 
 @onready
 var button_return: Button = $CenterContainer/VBoxContainer/Button
+@onready
+var label_winner: RichTextLabel = $CenterContainer/VBoxContainer/Winner
+@onready
+var grid_stats: GridContainer = $CenterContainer/VBoxContainer/PlayerStats
 
 func _ready() -> void:
 	button_return.pressed.connect(_on_return)
 	
 func _on_return():
 	pass
+
+func display_stats(victor: int, player_stats: Array[Player.PlayerStats]):
+	label_winner.text = ColorController.color_text("VICTORY COMMANDER " + str(victor), ColorController.player_primary_colors[victor])
+	
+	var vss = PrefabController.get_prefab("Menus.VictoryScreenStat")
+	for p in player_stats:
+		var stat: VictoryScreenStat = vss.instantiate()
+		stat.fill_stats(p)
+		grid_stats.add_child(stat)
---
+++ Scripts/Menus/VictoryScreenStat.gd
@@ -0,0 +1,8 @@
+class_name VictoryScreenStat
+extends Node
+
+@onready
+var label_name: RichTextLabel = $Name
+
+func fill_stats(stats: Player.PlayerStats):
+	pass
