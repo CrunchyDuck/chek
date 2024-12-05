1a7be9be9b0921680fec3c94a2ab28fee0149a64
CrunchyDuck <realvulpes@gmail.com>
2024-12-05 23:24:47 +0000

    Fix more display errors on victory screen

--- Scripts/Menus/VictoryScreenStat.gd
+++ Scripts/Menus/VictoryScreenStat.gd
@@ -21,7 +21,7 @@ const num_misc_to_display = 2
 
 func fill_stats(_stats: Player.PlayerStats):
 	self.stats = _stats
-	label_name.text = ColorController.color_text(player.name, pcol)
+	label_name.text = ColorController.color_text(player.character_name, pcol)
 	
 	add_killed()
 	add_lost()
@@ -41,50 +41,62 @@ func fill_stats(_stats: Player.PlayerStats):
 func add_killed():
 	var l = label_subfield.duplicate()
 	l.text = "KILLED: " + ColorController.color_text(str(stats.pieces_killed), pcol)
+	add_child(l)
 
 func add_lost():
 	var l = label_subfield.duplicate()
 	l.text = "LOST: " + ColorController.color_text(str(stats.pieces_lost), pcol)
+	add_child(l)
 
 func add_turns_taken():
 	var l = label_subfield.duplicate()
 	l.text = "TURNS: " + ColorController.color_text(str(stats.turns_taken), pcol)
+	add_child(l)
 
 func add_distance():
 	var l = label_subfield.duplicate()
 	l.text = "MOVED: " + ColorController.color_text(str(stats.distance_moved), pcol)
+	add_child(l)
 	
 func add_average_turn():
 	var l = label_subfield.duplicate()
 	l.text = "AVG TRN TIME: " + ColorController.color_text(str(stats.average_turn_time), pcol)
+	add_child(l)
 #endregion
 
 #region Misc stats
 func add_mistakes():
 	var l = label_subfield.duplicate()
 	l.text = "MISTAKES: " + ColorController.color_text(str(stats.mistakes), pcol)
+	add_child(l)
 	
 func add_tricks():
 	var l = label_subfield.duplicate()
 	l.text = "TRICKS: " + ColorController.color_text(str(stats.tricks_pulled), pcol)
+	add_child(l)
 
 func add_lights_on():
 	var l = label_subfield.duplicate()
 	l.text = "HAD LIGHT ON: " + ColorController.color_text(str(stats.lights_on), pcol)
+	add_child(l)
 
 func add_ventilation_on():
 	var l = label_subfield.duplicate()
 	l.text = "HAD VENT ON: " + ColorController.color_text(str(stats.ventilation_on), pcol)
+	add_child(l)
 
 func add_monitor_off():
 	var l = label_subfield.duplicate()
 	l.text = "TRNED OFF MONITOR: " + ColorController.color_text(str(stats.times_turned_monitor_off), pcol)
+	add_child(l)
 
 func add_happy():
 	var l = label_subfield.duplicate()
 	l.text = "HAPPY: " + ColorController.color_text(str(stats.happy), pcol)
+	add_child(l)
 
 func add_cheated():
 	var l = label_subfield.duplicate()
 	l.text = "CHEATED: " + ColorController.color_text(str(stats.cheated), pcol)
+	add_child(l)
 #endregion
