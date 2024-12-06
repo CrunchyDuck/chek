6fe489f774e4124bf9c5f1c78fbe25e7c23a5aec
CrunchyDuck <realvulpes@gmail.com>
2024-12-06 11:53:07 +0000

    Add function to change the texture of ButtonLight at runtime

--- Scripts/Model/ButtonLight.gd
+++ Scripts/Model/ButtonLight.gd
@@ -9,10 +9,22 @@ var lit_texture: StandardMaterial3D
 @export
 var material_index: int
 
+var lit: bool = false
+
 func toggle_power(state: bool):
 	if target == null:
 		return
 	if state:
+		lit = true
 		target.set_surface_override_material(material_index, lit_texture)
 	else:
+		lit = false
 		target.set_surface_override_material(material_index, unlit_texture)
+
+func change_lit_texture(new_texture: StandardMaterial3D):
+	lit_texture = new_texture
+	toggle_power(lit)
+
+func change_unlit_texture(new_texture: StandardMaterial3D):
+	unlit_texture = new_texture
+	toggle_power(lit)
