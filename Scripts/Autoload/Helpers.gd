bad8943e60ebdf2f8e94abe4351833bd90fd2f2c
CrunchyDuck <realvulpes@gmail.com>
2024-12-06 10:11:59 +0000

    Make Helpers.enable/disable_node propagate to all children

--- Scripts/Autoload/Helpers.gd
+++ Scripts/Autoload/Helpers.gd
@@ -11,15 +11,19 @@ static func destroy_node(node: Node):
 	node.queue_free()
 
 static func disable_node(node: Node):
-	node.process_mode = Node.PROCESS_MODE_DISABLED
 	if node.has_method("hide"):
 		node.hide()
 	if node.has_method("on_disable"):
 		node.on_disable()
+	node.process_mode = Node.PROCESS_MODE_DISABLED
+	for child in node.get_children():
+		disable_node(child)
 
 static func enable_node(node: Node):
-	node.process_mode = Node.PROCESS_MODE_INHERIT
 	if node.has_method("show"):
 		node.show()
 	if node.has_method("on_enable"):
 		node.on_enable()
+	node.process_mode = Node.PROCESS_MODE_INHERIT
+	for child in node.get_children():
+		enable_node(child)
