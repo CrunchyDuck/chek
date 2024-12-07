b4ff8d63cb34f72e6ef7cdea8a3a08c3528f933b
CrunchyDuck <realvulpes@gmail.com>
2024-12-07 12:38:58 +0000

    Start of model porting - using .blend format has caused irreconcilable problems

--- Scripts/Nameplate.gd
+++ Scripts/Nameplate.gd
@@ -5,7 +5,6 @@ var node_name: Label3D = $Name
 @onready
 var node_job: Label3D = $Job
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  node_name.text = GameController.character_name
-  node_job.text = GameController.job_name
+	node_name.text = GameController.character_name
+	node_job.text = GameController.job_name
