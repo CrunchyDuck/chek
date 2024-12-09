37db0ab3ce1a55d8c3e7f8b2b32eb7a8b43142ba
CrunchyDuck <realvulpes@gmail.com>
2024-12-09 05:07:37 +0000

    Allow going backwards in sacred piece selection with right click

--- Scripts/Menus/Setup/SacredPieceSelector.gd
+++ Scripts/Menus/Setup/SacredPieceSelector.gd
@@ -2,13 +2,23 @@ class_name SacredPieceSelector
 extends TextureButton
 
 var current_piece: ChessPiece.ePieces = ChessPiece.ePieces.King
+var last_button: MouseButton = 0
+
 
 func _ready():
 	pressed.connect(next_piece)
 	_set_texture(current_piece)
 
+func _input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	last_button = event.button_index
+
 func next_piece():
-	current_piece = wrapi(current_piece + 1, 0, ChessPiece.ePieces.size()) as ChessPiece.ePieces
+	if last_button == MOUSE_BUTTON_LEFT:
+		current_piece = wrapi(current_piece + 1, 0, ChessPiece.ePieces.size()) as ChessPiece.ePieces
+	elif last_button == MOUSE_BUTTON_RIGHT:
+		current_piece = wrapi(current_piece - 1, 0, ChessPiece.ePieces.size()) as ChessPiece.ePieces
 	if current_piece == ChessPiece.ePieces.Blocker:
 		next_piece()
 		return
